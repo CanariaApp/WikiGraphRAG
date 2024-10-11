a73370725e16861fbe51c71757c3f9e853a8957c
Sinan Kefeli <sinan@decanaria.com>
2024-10-10 20:53:01 -0700

    neo4j insertion pipeline

--- deploy/insert_neo4j.sh
+++ deploy/insert_neo4j.sh
@@ -3,6 +3,7 @@ docker compose run --rm wikidump5x-neo4j bin/neo4j-admin database import full \
     --nodes=Paragraphs=/var/lib/neo4j/import/embeddings/nodes/embedding/.*.csv \
     --nodes=Pages=/var/lib/neo4j/import/embeddings/nodes/title.csv \
     --relationships=BELONGS=/var/lib/neo4j/import/embeddings/edges/embedding_pages/.*.csv \
+    --relationships=LINKED=/var/lib/neo4j/import/embeddings/edges/title_title.csv \
     --array-delimiter=';' \
     --auto-skip-subsequent-headers=true \
     --skip-bad-relationships \
